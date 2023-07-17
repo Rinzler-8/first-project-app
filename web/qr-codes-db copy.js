042bbad876b807092027b5026930db893c709799
Rinzler-8 <madboss1801@gmail.com>
2023-07-17 15:07:18 +0700

    mongo v1

--- web/qr-codes-db copy.js
+++ web/qr-codes-db copy.js
@@ -2,24 +2,12 @@
   This file interacts with the app's database and is used by the app's REST APIs.
 */
 
-import { MongoClient } from "mongodb";
-import { ObjectId } from "mongodb";
+import sqlite3 from "sqlite3";
+import path from "path";
 import shopify from "./shopify.js";
 
-const uri = "mongodb://localhost:27017"; // Replace with your MongoDB URI
-const client = new MongoClient(uri);
+const DEFAULT_DB_FILE = path.join(process.cwd(), "qr_codes_db.sqlite");
 const DEFAULT_PURCHASE_QUANTITY = 1;
-
-async function connectToDB() {
-  try {
-    await client.connect();
-    console.log("Connected to MongoDB!");
-    QRCodesDB.ready = Promise.resolve();
-    return client.db("qr_db").collection("qr_codes"); // Return the 'qr_codes' collection directly
-  } catch (err) {
-    console.error("Error connecting to MongoDB:", err);
-  }
-}
 
 export const QRCodesDB = {
   qrCodesTableName: "qr_codes",
@@ -38,7 +26,14 @@ export const QRCodesDB = {
   }) {
     await this.ready;
 
-    const document = {
+    const query = `
+      INSERT INTO ${this.qrCodesTableName}
+      (shopDomain, title, productId, variantId, handle, discountId, discountCode, destination, scans)
+      VALUES (?, ?, ?, ?, ?, ?, ?, ?, 0)
+      RETURNING id;
+    `;
+
+    const rawResults = await this.__query(query, [
       shopDomain,
       title,
       productId,
@@ -47,12 +42,9 @@ export const QRCodesDB = {
       discountId,
       discountCode,
       destination,
-      scans: 0,
-      createdAt: new Date(),
-    };
+    ]);
 
-    const result = await this.db.insertOne(document);
-    return result.insertedId;
+    return rawResults[0].id;
   },
 
   update: async function (
@@ -69,46 +61,64 @@ export const QRCodesDB = {
   ) {
     await this.ready;
 
-    const query = { _id: ObjectId(id) };
-    const updateDocument = {
-      $set: {
-        title,
-        productId,
-        variantId,
-        handle,
-        discountId,
-        discountCode,
-        destination,
-      },
-    };
+    const query = `
+      UPDATE ${this.qrCodesTableName}
+      SET
+        title = ?,
+        productId = ?,
+        variantId = ?,
+        handle = ?,
+        discountId = ?,
+        discountCode = ?,
+        destination = ?
+      WHERE
+        id = ?;
+    `;
 
-    await this.db.updateOne(query, updateDocument);
+    await this.__query(query, [
+      title,
+      productId,
+      variantId,
+      handle,
+      discountId,
+      discountCode,
+      destination,
+      id,
+    ]);
     return true;
   },
 
   list: async function (shopDomain) {
     await this.ready;
+    const query = `
+      SELECT * FROM ${this.qrCodesTableName}
+      WHERE shopDomain = ?;
+    `;
 
-    const query = { shopDomain };
-    const results = await this.db.find(query).toArray();
+    const results = await this.__query(query, [shopDomain]);
 
     return results.map((qrcode) => this.__addImageUrl(qrcode));
   },
 
   read: async function (id) {
     await this.ready;
+    const query = `
+      SELECT * FROM ${this.qrCodesTableName}
+      WHERE id = ?;
+    `;
+    const rows = await this.__query(query, [id]);
+    if (!Array.isArray(rows) || rows?.length !== 1) return undefined;
 
-    const query = { _id: ObjectId(id) };
-    const qrcode = await this.db.findOne(query);
-
-    return qrcode ? this.__addImageUrl(qrcode) : undefined;
+    return this.__addImageUrl(rows[0]);
   },
 
   delete: async function (id) {
     await this.ready;
-
-    const query = { _id: ObjectId(id) };
-    await this.db.deleteOne(query);
+    const query = `
+      DELETE FROM ${this.qrCodesTableName}
+      WHERE id = ?;
+    `;
+    await this.__query(query, [id]);
     return true;
   },
 
@@ -119,11 +129,13 @@ export const QRCodesDB = {
 
   /* The behavior when a QR code is scanned */
   handleCodeScan: async function (qrcode) {
+
     /* Log the scan in the database */
     await this.__increaseScanCount(qrcode);
 
     const url = new URL(qrcode.shopDomain);
     switch (qrcode.destination) {
+
       /* The QR code redirects to the product view */
       case "product":
         return this.__goToProductView(url, qrcode);
@@ -140,35 +152,66 @@ export const QRCodesDB = {
   /* Private */
 
   /*
-    Used to check whether to create the collection.
-    Also used to make sure the collection is set up before the server starts.
+    Used to check whether to create the database.
+    Also used to make sure the database and table are set up before the server starts.
   */
 
-  __hasQrCodesCollection: async function () {
-    const collections = await this.db
-      .listCollections({ name: this.qrCodesTableName })
-      .toArray();
-    return collections.length > 0;
+  __hasQrCodesTable: async function () {
+    const query = `
+      SELECT name FROM sqlite_schema
+      WHERE
+        type = 'table' AND
+        name = ?;
+    `;
+    const rows = await this.__query(query, [this.qrCodesTableName]);
+    return rows.length === 1;
   },
 
+  /* Initializes the connection with the app's sqlite3 database */
   init: async function () {
-    try {
-      this.db = await connectToDB(); // Initialize the db property with the connected database
 
-      const hasQrCodesCollection = await this.__hasQrCodesCollection();
+    /* Initializes the connection to the database */
+    this.db = this.db ?? new sqlite3.Database(DEFAULT_DB_FILE);
 
-      if (!hasQrCodesCollection) {
-        const indexes = [
-          { key: { shopDomain: 1 } },
-          { key: { createdAt: 1 }, expireAfterSeconds: 86400 }, // Auto-delete documents after 24 hours
-        ];
+    const hasQrCodesTable = await this.__hasQrCodesTable();
 
-        await this.db.createCollection(this.qrCodesTableName);
-        await this.db.createIndexes(this.qrCodesTableName, indexes);
-      }
-    } catch (err) {
-      console.error("Error initializing MongoDB:", err);
+    if (hasQrCodesTable) {
+      this.ready = Promise.resolve();
+
+      /* Create the QR code table if it hasn't been created */
+    } else {
+      const query = `
+        CREATE TABLE ${this.qrCodesTableName} (
+          id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+          shopDomain VARCHAR(511) NOT NULL,
+          title VARCHAR(511) NOT NULL,
+          productId VARCHAR(255) NOT NULL,
+          variantId VARCHAR(255) NOT NULL,
+          handle VARCHAR(255) NOT NULL,
+          discountId VARCHAR(255) NOT NULL,
+          discountCode VARCHAR(255) NOT NULL,
+          destination VARCHAR(255) NOT NULL,
+          scans INTEGER,
+          createdAt DATETIME NOT NULL DEFAULT (datetime(CURRENT_TIMESTAMP, 'localtime'))
+        )
+      `;
+
+      /* Tell the various CRUD methods that they can execute */
+      this.ready = this.__query(query);
     }
+  },
+
+  /* Perform a query on the database. Used by the various CRUD methods. */
+  __query: function (sql, params = []) {
+    return new Promise((resolve, reject) => {
+      this.db.all(sql, params, (err, result) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(result);
+      });
+    });
   },
 
   __addImageUrl: function (qrcode) {
@@ -186,11 +229,12 @@ export const QRCodesDB = {
   },
 
   __increaseScanCount: async function (qrcode) {
-    const query = { _id: ObjectId(qrcode.id) };
-    const updateDocument = {
-      $inc: { scans: 1 },
-    };
-    await this.db.updateOne(query, updateDocument);
+    const query = `
+      UPDATE ${this.qrCodesTableName}
+      SET scans = scans + 1
+      WHERE id = ?
+    `;
+    await this.__query(query, [qrcode.id]);
   },
 
   __goToProductView: function (url, qrcode) {
